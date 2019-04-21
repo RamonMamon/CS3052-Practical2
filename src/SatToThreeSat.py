9082b1119baffb16da61f6781e0795b9e2ec9468
RamonMamon <35195972+RamonMamon@users.noreply.github.com>
2019-04-21 14:57:31 +0100

    Almost all tests perfect. Just need to implement checking of comments after preamble.

--- src/SatToThreeSat.py
+++ src/SatToThreeSat.py
@@ -1,19 +1,27 @@
 import sys
 
 class Literal:
-    is_negative = False
-    variable = 0
     def __init__(self, variable):
+        self.is_negative = False
+        self.variable = 0
+
         if '--' in variable:
             raise ValueError
         elif variable[0] == '-':
+            # Storing the number after the negative value to allow comparison
             self.is_negative = True
-        self.variable = int(variable[1:])
+            self.variable = int(variable[1:])
+        else:
+            self.variable = int(variable)    
 
-    def get_variable(self):
+    def value(self):
         if self.is_negative:
-            return -variable
-        return variable
+            return -1 * self.variable
+        return self.variable
+
+    def __str__(self):
+        # print ('TO STRING')
+        return str(self.value())
 
     # Checks if the other literal has the same variable.
     def __eq__(self, that):
@@ -23,133 +31,155 @@ class Literal:
         
         
 class Clause:
-    literals = []
+    # literals = []
+    # num_literals = 0
+    def __init__(self):
+        self.literals = []
 
-    def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+    def insert_literal(self, literal):
+        # Check each character and see if it's already saved
+        # if literal != '0' and literal not in self.literals:
+        self.literals.append(literal)
+            # self.num_literals += 1
 
+    def get_literal(self, index):
+        return self.literals[index]
+
+    def pop(self, index = -1):
+        return self.literals.pop(index)
+    def __str__(self):
+        string = ''
+        for literal in self.literals:
+            string += str(literal) + ' '
+        return string
+
+
+    # Make a to_string function
 def read_initial():
 
     # implement a method of reading a command line argument which specifies
     # the type of operation that is going to be conducted.
     # This allows me to implement everything in just one file but also
     # allows me to create multiple executibles using a makefile with different args
-
-    
+    output = []
+    output_format = ''
+    num_lines = 0
+    num_vars = 0
     try:
-        output_format = ''
-        num_lines = 0
-        num_vars = 0
+        # tempLime = sys.stdin.read()
 
+        # print (tempLime)
         while True:
             line = sys.stdin.readline().rstrip()
             line_val = line.split()
-
+            # print (line)
             if len(line_val) == 0:
                 continue
-            else:
-                linetype = line_val[0]
+
+            linetype = line[0]
 
             if linetype == 'p':
                 # If output format is cnf then use to_sat3 
                 # else use to_cnf
                 output_format = line_val[1]
-                # if output_format == 'cnf':
-                num_lines = int(line_val[2])
-                num_vars = int(line_val[3])
-                to_sat3(num_lines, num_vars)
+                if output_format != 'cnf':
+                    raise ValueError
+                num_vars = int(line_val[2])
+                num_lines = int(line_val[3])
+                to_sat3(num_vars, num_lines, output)
                 break
             elif linetype != 'c':
                 raise ValueError
     except ValueError:
         print('Invalid Format')
-        sys.exit(1)
+        exit(1)
+        
 
 
-def to_sat3(num_lines, num_vars):
+def to_sat3(num_vars, num_lines, output):
 
     # Read SAT in CNF from stdin
     # Check the first character of every line
-    output = []
-    literals = []
-    new_var_index = 1
-    i = 0
+    variables = []
+    clauses = []
+    new_var_index = num_vars + 1
     
     while True:
-        if i > num_lines:
-            raise ValueError    
-        line = sys.stdin.readline().rstrip()
-        line_vars = line.split()
+        # Stores each clause 
+        # print("Hello")
+        line_vars = sys.stdin.readline().strip().split()
+        if len(line_vars) == 0:
+            break
+        # print (len(line_vars))
+        clauses.append(line_vars)
+        if len(clauses) > num_lines:
+            raise ValueError
 
-        lit_index = 0
-        output_line = []
-
-        # Want to append characters to an output line
-
+    for line in clauses:
+        # Parses each line 
+        literal_index = 0
+        clause = Clause()      
         while True:
-            if len(line_vars) == 0:
+            if len(line) == 0:
                 break
-            literal = line_vars.pop(0)
-
+            
+            # Gets the character at the first index
+            literal = Literal(line.pop(0))
+            
             # Check each character and see if it's already saved
-
-            if literal != '0' and literal not in literals:
-                literals.append(literal)
+            if literal.value() != 0 and literal not in variables:
+                variables.append(literal)
 
             # Check if the number of variables is greater than num_vars
-
-            if len(literals) > num_vars:
+            if len(variables) > num_vars:
                 raise ValueError
-
-            # TODO check if the literals have a double negative.
             
-            if literal == '0':
+            if literal.value() == 0:
+                # Creates a new clause when the literal is 0
+                clause.insert_literal(literal)
+                output.append(clause)
+                clause = Clause()
+                literal_index = 0
+                # num_lines += 1
 
-                # Creates a new line when the literal is 0
-
-                output_line.append('0')
-                output.append(output_line)
-                output_line = []
-                lit_index = 0
-            elif lit_index % 3 == 0 and lit_index != 0:
-
+            elif literal_index % 3 == 0 and literal_index != 0:
                 # Splits the clauses if the number of clauses is greater than 3
 
-                # Returns the third and fourth index to the beginning of the list of Line literals
+                # Returns the third and fourth literals to the beginning of the list of Line literals
+                third = str(literal)
+                fourth = str(clause.pop())
 
-                line_vars.insert(0, literal)
-                line_vars.insert(0, output_line.pop())
+                line.insert(0, third)
+                line.insert(0, fourth)
 
-                newVar = 'y' + str(new_var_index)
-                output_line.append(newVar)
-                output_line.append('0')
+                var_string = str(new_var_index)
+                new_variable = Literal(var_string)
+                clause.insert_literal(new_variable)
+                clause.insert_literal(Literal('0'))
 
-                # Appends the negation of the new variable to the list of line literals
+                line.insert(0,'-' + var_string)
 
-                line_vars.append('-' + newVar)
-                output.append(output_line)
-                output_line = []
-
-                # Increment 1 to the number of vars to compensate for the newly added negative variable
-
+                output.append(clause)
+                clause = Clause()
+                
+                num_lines += 1
                 num_vars += 1
                 new_var_index += 1
-                lit_index = 0
+                literal_index = 0
+
             else:
-
                 # Appends the literal to the output_line
+                clause.insert_literal(literal)
+                if len(line) == 0:
+                    clause.insert_literal(Literal('0'))
+                    output.append(clause)
+                literal_index += 1  
 
-                # Catches double negatives.
-                if '--' in literal:
-                    raise ValueError
+    if num_lines > len(output):
+        raise ValueError
 
-                output_line.append(literal)
-                if len(line_vars) == 0:
-                    output_line.append('0')
-                    output.append(output_line)
-                lit_index += 1
-        i += 1
-    for line in output:
-        print(' '.join(line))
+    print ('p cnf ' + str(len(variables)) + ' ' + str(len(output)))
+    for clause in output:
+        print(clause)
 
 read_initial()
